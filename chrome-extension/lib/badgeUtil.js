78b255899abe71ffd309b0c5537c5f3e38c213c4
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-03-13 17:28:44 +0900

    [Add #1] 今日の保存件数でバッジテキストの背景色を変更
    今日の保存0件: 赤
    今日の保存あり: デフォルト色（Chrome56現在少し薄い青）

--- chrome-extension/lib/badgeUtil.js
+++ chrome-extension/lib/badgeUtil.js
@@ -1,9 +1,23 @@
 
 const badgeUtil = {
+	_defaultBackgroundColor: [0, 0, 0, 0],
+	_changeBackgroundColorByTodaySavedStatus: tabSavedTimes => {
+		const todayDateString = new Date().toDateString();
+		const isSavedToday = tabSavedTimes.some(tabSavedTimeStr => {
+			const tabSavedTime = parseInt(tabSavedTimeStr);
+			const date = new Date(tabSavedTime);
+			return todayDateString === date.toDateString();
+		});
+		const color = isSavedToday ? badgeUtil._defaultBackgroundColor : "red";
+		chrome.browserAction.setBadgeBackgroundColor({
+			color
+		});
+	},
 	show: tabSavedTimes => {
 		const count = tabSavedTimes.length;
 		chrome.browserAction.setBadgeText({
 			text: String(count)
 		});
+		badgeUtil._changeBackgroundColorByTodaySavedStatus(tabSavedTimes);
 	}
 };
