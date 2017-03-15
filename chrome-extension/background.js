dccfd48632b9f374196d9f4e218aa5a29118237d
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-03-15 23:30:58 +0900

    [Fix #1] 無駄なalarmの再設定処理修正
    （alarmによる）イベントページの起動のたびにalarm再設定していた

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -6,18 +6,20 @@ const showHistoryCountBadge = () => {
 	});
 };
 
-chrome.runtime.onInstalled.addListener(showHistoryCountBadge);
+chrome.runtime.onInstalled.addListener(() => {
+	showHistoryCountBadge();
+
+	chrome.alarms.create("every_midnight", {
+		// 毎日深夜0時
+		// 過去を指定しても繰り返し間隔を考慮して適切なタイミングを自動計算してくれる
+		when: new Date("2017/01/01 00:00:00").getTime(),
+		periodInMinutes: 60 * 24
+	});
+});
 
 
 chrome.alarms.onAlarm.addListener(alarm => {
 	if (alarm.name === "every_midnight") {
 		showHistoryCountBadge();
 	}
-});
-
-chrome.alarms.create("every_midnight", {
-	// 毎日深夜0時
-	// 過去を指定しても繰り返し間隔を考慮して適切なタイミングを自動計算してくれる
-	when: new Date("2017/01/01 00:00:00").getTime(),
-	periodInMinutes: 60 * 24
 });
