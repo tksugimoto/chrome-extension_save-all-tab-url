fded496ac9bfa47ec2b2a1af3b3d5046f67d69ec
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2020-03-08 00:37:10 +0900

    refactor: ES Module化に伴う整理
    スコープが別になったのでprivate変数を衝突回避のためにobjectに入れる必要がなくなった

--- chrome-extension/lib/badgeUtil.js
+++ chrome-extension/lib/badgeUtil.js
@@ -1,34 +1,37 @@
 
-const badgeUtil = {
-	_defaultBrowserActionTitle: chrome.runtime.getManifest().browser_action.default_title,
-	_defaultBackgroundColor: [0, 0, 0, 0],
-	_changeBadgeInfoByTodaySavedStatus: tabSavedTimes => {
-		const todayDateString = new Date().toDateString();
-		const isSavedToday = tabSavedTimes.some(tabSavedTimeStr => {
-			const tabSavedTime = parseInt(tabSavedTimeStr);
-			const date = new Date(tabSavedTime);
-			return todayDateString === date.toDateString();
-		});
-		const color = isSavedToday ? badgeUtil._defaultBackgroundColor : 'red';
-		chrome.browserAction.setBadgeBackgroundColor({
-			color,
-		});
-		const count = tabSavedTimes.length;
-		const title = badgeUtil._defaultBrowserActionTitle + `
-			保存件数: ${count}件
-			今日の保存: ${isSavedToday ? 'あり（青）' : 'なし（赤）'}
-			`.replace(/^\t+/mg, '');
-		chrome.browserAction.setTitle({
-			title,
-		});
-	},
-	show: tabSavedTimes => {
-		const count = tabSavedTimes.length;
-		chrome.browserAction.setBadgeText({
-			text: String(count),
-		});
-		badgeUtil._changeBadgeInfoByTodaySavedStatus(tabSavedTimes);
-	},
+const _defaultBrowserActionTitle = chrome.runtime.getManifest().browser_action.default_title;
+
+const _defaultBackgroundColor = [0, 0, 0, 0];
+
+const _changeBadgeInfoByTodaySavedStatus = tabSavedTimes => {
+	const todayDateString = new Date().toDateString();
+	const isSavedToday = tabSavedTimes.some(tabSavedTimeStr => {
+		const tabSavedTime = parseInt(tabSavedTimeStr);
+		const date = new Date(tabSavedTime);
+		return todayDateString === date.toDateString();
+	});
+	const color = isSavedToday ? _defaultBackgroundColor : 'red';
+	chrome.browserAction.setBadgeBackgroundColor({
+		color,
+	});
+	const count = tabSavedTimes.length;
+	const title = _defaultBrowserActionTitle + `
+		保存件数: ${count}件
+		今日の保存: ${isSavedToday ? 'あり（青）' : 'なし（赤）'}
+		`.replace(/^\t+/mg, '');
+	chrome.browserAction.setTitle({
+		title,
+	});
 };
 
-export default badgeUtil;
+const show = tabSavedTimes => {
+	const count = tabSavedTimes.length;
+	chrome.browserAction.setBadgeText({
+		text: String(count),
+	});
+	_changeBadgeInfoByTodaySavedStatus(tabSavedTimes);
+};
+
+export default {
+	show,
+};
